57406811ecfbfea98c03c68aa96eb9f57e5c898a
ray_arwindra <wan.muhammad81@ui.ac.id>
2020-12-23 19:20:42 +0700

    Made simple mouse driver

---
+++ driver.sh
@@ -0,0 +1,45 @@
+# !/bin/bash
+# conf.sh
+# FOR: configuring linux device driver psmouse
+
+PASSWORD=sysprog2019
+
+changeres() {
+    echo '-------------------------------------------------'
+    echo '                    PSMOUSE                      '
+    echo '-------------------------------------------------'
+    read -p 'Resolution (in dpi): ' res
+    echo $PASSWORD | sudo -S modprobe -r psmouse
+    echo $PASSWORD | sudo -S modprobe psmouse resolution=$res
+    echo The resolution of the mouse is now: $(cat /sys/module/psmouse/parameters/resolution)
+    read -p 'Press [Enter] key to continue...'
+    mainmenu
+}
+
+mainmenu() {
+    echo '================================================='
+    echo '                    MAIN MENU                    '
+    echo '================================================='
+    echo CURRENT PSMOUSE STATUS
+    echo resolution: $(cat /sys/module/psmouse/parameters/resolution)
+
+    COLUMNS=12
+    PS3='choose driver to configure 1-2: '
+    options=("Psmouse" "Exit")
+    select opt in "${options[@]}"
+    do
+        case $opt in
+            "Psmouse")
+                changeres
+                ;;
+            "Exit")
+                echo 'Bye Bye...'
+                read -p 'Press [Enter] key to continue...'
+                exit
+                ;;
+            *) echo "invalid option $REPLY";;
+        esac
+    done
+}
+
+mainmenu
